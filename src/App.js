3554ef1eee970e813681e48097a565f5ef275f67
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-02-02 11:28:45 +0100

    add mui library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Calcula tu coche</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Que todo <code>rula como tiene</code> que rular.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <main>
+        <h2>Aqui va a ir la app </h2>
+        <Button variant="contained">Hello World</Button>;
+      </main>
     </div>
   );
 }
