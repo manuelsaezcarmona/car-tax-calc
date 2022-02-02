d6959e6eb715609dd75f6d689e2cde03e78c1ec1
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-02-03 00:02:37 +0100

    add static data.js

---
+++ src/services/data.js
@@ -0,0 +1,46 @@
+export const brands = [
+  { label: 'Alfa Romeo', value: 'Alfa Romeo'},
+  { label: 'BMW', value: 'BMW'},
+  { label: 'Dacia', value: 'Dacia'},
+  { label: 'Fiat', value: 'Fiat'},
+  { label: 'Ford', value: 'Ford'},
+  { label: 'Hyundai', value: 'Hyundai'},
+  { label: 'Renault', value: 'Renault'}
+
+]
+
+export const fuel = [
+  { label: 'Diesel', value: 'D'},
+  { label: 'Electrico', value: 'Elc'},
+  { label: 'Gasolina', value: 'G'},
+  { label: 'Super', value: 'S'},
+  { label: 'Hibrido Diesel', value: 'DyE'},
+  { label: 'Hibrido Gasolina', value: 'GyE'}
+]
+
+export const labels =
+     {
+      brand: "Marca",
+      model: "Modelo",
+      period: "2015-2020",
+      cc: "Cilindrada",
+      cylinders: "Cilindros",
+      fuel: "Combustivle",
+      kw: "Potencia (kW)",
+      cvf: "Potencia Fiscal",
+      cv: "Potencia (CV)",
+      value: "Valor"
+    }
+
+export const sampleInfoCar = [
+
+]
+
+export const sampleTaxValue = [
+  {year:"2019", value:28500 },
+  {year:"2020", value:26650 },
+  {year:"2021", value:22800 },
+  {year:"2022", value:19950 },
+  {year:"2023", value:17100 },
+  {year:"2024", value:14250 }
+]
