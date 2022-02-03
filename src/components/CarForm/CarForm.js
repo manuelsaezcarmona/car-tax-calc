81f9f1b353eb56061f6e58b58b5862f63049d059
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-02-03 10:53:10 +0100

    conect data to CarForm

--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
+import { brands, fuel, sampleModels } from '../../services/data'
 
 export function CarForm() {
   return (
   <div className='CarForm-container'>
+
+
+
       <Form className='car-form'>
          <Form.Group className="car-form-item" >
            <Form.Label>Fecha de Primera Matriculación</Form.Label>
@@ -12,34 +16,27 @@ export function CarForm() {
         <Form.Group className="car-form-item" >
           <Form.Label>Marca</Form.Label>
             <Form.Select className= "car-form-item__select" >
-                <option value="Audi">Audi</option>
-                <option value="BMW">BMW</option>
-                <option value="Dacia">Dacia</option>
-                <option value="Hyundai">Hyundai</option>
+              {brands.map((brand => (
+                <option key={brand} value={brand}>{brand}</option>
+              )))}
             </Form.Select>
       </Form.Group>
 
       <Form.Group className="car-form-item" >
-          <Form.Label>Marca</Form.Label>
+          <Form.Label>Combustible</Form.Label>
             <Form.Select className= "car-form-item__select" >
-                <option value="D">Diesel</option>
-                <option value="Elc">Electrico</option>
-                <option value="G">Gasolina</option>
-                <option value="S">Super</option>
-                <option value="DyE">Hibrido Diesel</option>
-                <option value="GyE">Hibrido Gasolina</option>
+                   {fuel.map((fuelitem) => (
+                <option key={fuelitem.value} value={fuelitem.value}>{fuelitem.label}</option>
+              ))}
             </Form.Select>
       </Form.Group>
 
        <Form.Group className="car-form-item" >
           <Form.Label>Modelo</Form.Label>
             <Form.Select className= "car-form-item__select" >
-                <option value="D">GIULIA 2.2 Diesel Aut. 8V 150l</option>
-                <option value="Elc">GIULIA 2.2 Diesel Super 6V 180</option>
-                <option value="G">GIULIA 2.2 Diesel Veloce Q4 Aut. 8V 210</option>
-                <option value="S">GIULIETTA 1.6 JTD Super / JTDm SS 6V 120</option>
-                <option value="DyE">Giuliietta 1.6 JTD 120CV B-TECH</option>
-                <option value="GyE">Giuliietta 1.6 JTD 120CV SPORT</option>
+                {sampleModels.map((model => (
+                <option key={model} value={model}>{model}</option>
+              )))}
             </Form.Select>
       </Form.Group>
 
