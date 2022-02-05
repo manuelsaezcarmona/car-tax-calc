545e4a0c2a93fb99fed5389f59124c31b1097d6d
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-02-05 19:54:36 +0100

    add cars context to carInfo Component

--- src/components/CarInfo/CarInfo.js
+++ src/components/CarInfo/CarInfo.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Table } from 'react-bootstrap';
+import { VehicleContext } from '../../context/vehicle.context'
 
 export function CarInfo() {
 
-// const { vehicle } = useContext(CarsContext);
+ const { vehicle } = useContext(VehicleContext);
 
   return (
       <div className='car-general-info'>
@@ -16,36 +17,36 @@ export function CarInfo() {
             <tbody>
                 <tr>
                   <td className='car-general-info__label'>Marca</td>
-                  <td className='car-general-info__data'>BMW</td>
+                  <td className='car-general-info__data'>{vehicle.brand}</td>
                 </tr>
                   <tr>
                   <td className='car-general-info__label'>Modelo</td>
-                  <td className='car-general-info__data'>X1 sDrive16d</td>
+                  <td className='car-general-info__data'>{vehicle.model}</td>
                 </tr>
                   <tr>
                   <td className='car-general-info__label'>Cilindrada</td>
-                  <td className='car-general-info__data'>1496</td>
+                  <td className='car-general-info__data'>{vehicle.cc}</td>
                 </tr>
                   <tr>
                   <td className='car-general-info__label'>Cilindros</td>
-                  <td className='car-general-info__data'>3</td>
+                  <td className='car-general-info__data'>{vehicle.cylinders}</td>
                 </tr>
                   <tr>
                   <td className='car-general-info__label'>Combustible</td>
-                  <td className='car-general-info__data'>Diesel</td>
+                  <td className='car-general-info__data'>{vehicle.fuel}</td>
                 </tr>
                   <tr>
                   <td className='car-general-info__label'>Potencia(Kw)</td>
-                  <td className='car-general-info__data'>85</td>
+                  <td className='car-general-info__data'>{vehicle.kw}</td>
                 </tr>
 
                  <tr>
                   <td className='car-general-info__label'>Potencia Fiscal</td>
-                  <td className='car-general-info__data'>9,97</td>
+                  <td className='car-general-info__data'>{vehicle.cvf}</td>
                 </tr>
                 <tr>
                   <td className='car-general-info__label'>Potencia(CV)</td>
-                  <td className='car-general-info__data'>116</td>
+                  <td className='car-general-info__data'>{vehicle.cv}</td>
                 </tr>
             </tbody>
           </Table>
